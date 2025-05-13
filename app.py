33692bdede9c70167493258a30547b9bb7e126c7
antonialondono <alondono2@eafit.edu.co>
2025-05-13 15:47:50 -0500

    Update app.py

--- app.py
+++ app.py
@@ -313,7 +313,7 @@ else:
     
 # Footer mejorado
 st.markdown("""
-    <div style="background-color:#2c3e50; color:white; padding:1.5rem; border-radius:8px; margin-top:2rem;">
+    <div style="background-color:#ffb8f0; color:white; padding:1.5rem; border-radius:8px; margin-top:2rem;">
         <p style="text-align:center; margin-bottom:0;">
             <strong>Desarrollado para el análisis de datos de sensores urbanos</strong><br>
         </p>
